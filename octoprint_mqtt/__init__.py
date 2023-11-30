e80cfc9a8232829d17e7251071c62e810d122e92
webbariflo <145113593+webbariflo@users.noreply.github.com>
2023-11-30 18:55:23 +0530

    Update __init__.py

--- octoprint_mqtt/__init__.py
+++ octoprint_mqtt/__init__.py
@@ -5,49 +5,14 @@ import json
 import six
 import time
 from collections import deque
-
+import os
 import octoprint.plugin
 
 from octoprint.events import Events
 from octoprint.util import dict_minimal_mergediff
 
 
-class MqttPlugin(octoprint.plugin.SettingsPlugin,
-                 octoprint.plugin.StartupPlugin,
-                 octoprint.plugin.ShutdownPlugin,
-                 octoprint.plugin.EventHandlerPlugin,
-                 octoprint.plugin.ProgressPlugin,
-                 octoprint.plugin.TemplatePlugin,
-                 octoprint.plugin.AssetPlugin,
-                 octoprint.printer.PrinterCallback):
-
-    def __init__(self):
-        self._mqtt = None
-        self._mqtt_connected = False
-        self._mqtt_reset_state = True
-
-        self._mqtt_subscriptions = []
-
-        self._mqtt_publish_queue = deque()
-        self._mqtt_subscribe_queue = deque()
-
-        self.lastTemp = {}
-
-    def initialize(self):
-        self._printer.register_callback(self)
-
-        if self._settings.get(["broker", "url"]) is None:
-            self._logger.error("No broker URL defined, MQTT plugin won't be able to work")
-            return False
-
-    ##~~ TemplatePlugin API
-
-    def get_template_configs(self):
-        return [
-            dict(type="settings", name="MQTT")
-        ]
-
-
+class MqttPlugin(octoprint.printer.PrinterCallback):
     ##~~ PrinterCallback
 
     def on_printer_add_temperature(self, data):
